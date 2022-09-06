155e338c2b9a30bcdce5c78c5a3863c30b88280a
jibola2588 <jibolafawole258@gmail.com>
2022-09-06 14:03:32 -0700

    the form class

--- src/index.js
+++ src/index.js
@@ -1,23 +1,45 @@
-import React,{useState} from 'react'
+import React from 'react'
 import ReactDom from 'react-dom/client'
-import {FirstStateElement} from './components/First-State-Element'
+// import {FirstStateElement} from './components/First-State-Element'
 // import Jibola from './components/Jibola'
 import './index.css'
 // import {FirstStateClass } from './pages/First-State-Class'
 // import Jibola from './components/Jibola'
 
-import {Counter } from './pages/Counter'
+// import {Counter } from './pages/Counter'
+import {Form}  from './pages/Form'
+// import DynamicProps  from './pages/DynamicProps'
 
 const App = () => { 
-  const [show,setShow] =  useState(true)
+  // const [show,setShow] =  useState(true)
     return ( 
         <div className = "text-center">
+        <Form />
          {/* <FirstStateElement /> */}
         {/* <Jibola /> */}  
-
         {/* {show && <FirstStateClass/>  } 
         <button onClick = {() => setShow(false)} className = "p-4 bg-danger text-info">press me</button>   */}
-        <Counter />
+        {/* <Counter /> */}
+        {/* <div style ={{display:"flex" ,gap:'20px'}} >
+        <DynamicProps 
+        img = 'https://cdn.pixabay.com/photo/2012/03/04/01/01/father-22194_960_720.jpg'
+        name = "John Doe"
+        email = 'doe@yahoo.com'
+        phone = '070357809600'
+        />
+        <DynamicProps 
+                email = 'doe@yahoo.com'
+                phone = '070357809600'
+                fav = 'ball'
+                age = {24}
+        />
+        <DynamicProps 
+          img = "https://cdn.pixabay.com/photo/2021/02/11/05/34/woman-6004286_960_720.jpg"
+          fav = 'ball'
+          age = { 34 }
+          name = "folish son"
+        />
+        </div> */}
   </div>
     )
 }
---
+++ src/pages/DynamicProps.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+const DynamicProps = ({name,age,img,phone,email,fav}) => {
+  return (
+    <div className= "card" style = {{border:'1px solid teal',width:'330px',height:'500px'}}>
+     {img ? (<div className="top"style = {{width:'330px',height:'300px'}}>
+          <img 
+          style = {{objectFit:'cover',width:"100%",height:'100%'}}
+          src= {img} alt="image of someone"/>
+      </div>) : null}
+     <div className="bottom" style = {{padding:'0px 1rem'}}>
+     {name &&  <h4 className="name">name: 
+          <span className="span"  style = {{padding:'0px 0.25rem'}}>{name}</span>
+          </h4>}
+          {age && <h4 className="name">age: 
+          <span className="span" style = {{padding:'0px 0.25rem'}}>{age}</span>
+          </h4>}
+          {email && <h4 className="name">email: 
+          <span className="span" style = {{padding:'0px 0.25rem'}}>{email}</span>
+          </h4>}
+         {phone && <h4 className="name">phone: 
+          <span className="span" style = {{padding:'0px 0.25rem'}}>{phone}</span>
+          </h4>}
+         {fav && <h4 className="name">favourite: 
+          <span className="span" style = {{padding:'0px 0.25rem'}}>{fav}</span>
+          </h4>}
+      </div>
+    </div>
+  );
+}
+
+export default DynamicProps;
